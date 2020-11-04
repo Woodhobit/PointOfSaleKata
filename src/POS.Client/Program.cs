cc2e569f2fc94aa0ab918cc1124e910832e1dc4a
Oleksii Hoshii <oleksii.hoshii@flipdish.ie>
2020-11-04 23:12:07 +0300

    update demo

--- src/POS.Client/Program.cs
+++ src/POS.Client/Program.cs
@@ -16,9 +16,9 @@ namespace POS.Client
 
             await terminal.InitNewOrderAsync(Guid.NewGuid());
             await terminal.ScanAsync("A");
-            await terminal.ScanAsync("A");
-            await terminal.ScanAsync("A");
-            await terminal.ScanAsync("A");
+            await terminal.ScanAsync("B");
+            await terminal.ScanAsync("C");
+            await terminal.ScanAsync("D");
 
             var result = await terminal.CalculateTotalAsync();
         }
